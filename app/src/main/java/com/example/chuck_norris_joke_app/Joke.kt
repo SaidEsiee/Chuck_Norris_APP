e8afc6cb1f3b0cc70aa7bf7f37bca40c695dd163
SaidEsiee <said.oualla@edu.esiee.fr>
2022-05-11 17:03:55 +0200

    Chuck_Norris_Joke_App

---
+++ app/src/main/java/com/example/chuck_norris_joke_app/Joke.kt
@@ -0,0 +1,13 @@
+package com.example.chuck_norris_joke_app
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.StructureKind
+
+@Serializable
+data class Joke(val categories: List<String> = emptyList(),
+                @SerialName("created_at") val createdAt: String = "2020-01-05 13:42:29.296379",
+                @SerialName("icon_url") val iconUrl: String = "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+                val id: String = "p3GHoau2SLGTv04XhtIeeg",
+                @SerialName("updated_at") val updatedAt: String = "2020-01-05 13:42:29.296379",
+                val url: String = "https://api.chucknorris.io/jokes/p3GHoau2SLGTv04XhtIeeg",
+                val value:String = "Which came first - the chicken or the egg? Chuck Norris said it was the beer which came first, the hell with these damn animals!!!"
+)
--- app/src/main/java/com/example/chuck_norris_joke_app/JokeAdapter.kt
+++ app/src/main/java/com/example/chuck_norris_joke_app/JokeAdapter.kt
@@ -1,36 +1,44 @@
 package com.example.chuck_norris_joke_app
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chuck_norris_joke_app.JokeAdapter
 
 class JokeAdapter : RecyclerView.Adapter<JokeAdapter.JokeViewHolder>(){
-    class JokeViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class JokeViewHolder(val constraintLayout: ConstraintLayout) : RecyclerView.ViewHolder(constraintLayout){
+        val textView : TextView = constraintLayout.findViewById<TextView>(R.id.jokes)
+    }
 
     var list_jokes = listOf("The Pope once tried to bless Chuck Norris. Nobody crosses Chuck Norris.",
         "Chuck Norris uses a flamethrower to light his BBQ.",
-        "Chuck Norris gives computers viruses",
+        "Chuck Norris gives computers viruses.",
         "Chuck Norris can cover a Beatles song on Youtube and won't get comments saying he ruined the song.",
-        "There is only 1 person in the world who can kill Chuck Norris, ITS HIS EVIL TWIN Chack Norris",
+        "There is only 1 person in the world who can kill Chuck Norris, ITS HIS EVIL TWIN Chack Norris.",
         "Chuck Norris never asks if you are talking to him.",
-        "Chuck Norris once won an arm wrestling match,with both hands tied behind his back",
-        "Chuck Norris can connect parallel lines",
+        "Chuck Norris once won an arm wrestling match,with both hands tied behind his back.",
+        "Chuck Norris can connect parallel lines.",
         "Unlike Santa Claus, Chuck Norris doesn't need to check his list twice.",
         "Chuck Norris can login without signing up, on any website.")
 
+    var map_jokes = list_jokes.map {Joke()}
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        val textView = TextView(parent.context)
-        return JokeViewHolder(textView)
+        val view: ConstraintLayout = LayoutInflater.from(parent.context).inflate(R.layout.joke_layout,parent, false) as ConstraintLayout
+        /*val textView = TextView(parent.context)*/
+        return JokeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
-        val joke = list_jokes[position]
-        holder.textView.text = joke
+        val joke = map_jokes[position]
+        holder.textView.text = joke.toString()
     }
 
     override fun getItemCount(): Int {
-        return list_jokes.size
+        return map_jokes.size
     }
 }
 
